0c5422573d8fa4771ddf7cf2a13dada4b1f882f7
juzisang <995674251@qq.com>
2026-03-03 21:20:58 +0800

    remove useless code

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,28 +1,12 @@
 import { fetch } from "@tauri-apps/plugin-http";
 
-export interface RequestOptions {
-  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
-  headers?: Record<string, string>;
-  body?: any;
-  timeout?: number;
-  retry?: number;
-  baseURL?: string;
-}
-
-export interface ResponseData<T = any> {
-  data: T;
-  status: number;
-  statusText: string;
-  headers: Record<string, string>;
-}
-
 export default class RequestService {
   static async postBody(option: { url: string; data: Object }) {
     try {
       const response = await fetch(option.url, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json", // 根据需要设置正确的 Content-Type
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(option.data || {}),
       });
